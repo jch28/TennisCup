6bc25ff356ecba42d4c7cbabb4300849b5f8501a
Jeremy Chen <jeremychen@Jeremys-MacBook-Pro.local>
2019-11-01 21:31:45 +1100

    added pagination

--- src/Components/History/History.js
+++ src/Components/History/History.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import { Menu, Segment, Grid, Header, Icon, Divider, Search, Button, Dropdown, Table } from 'semantic-ui-react'
+import { Menu, Segment, Grid, Header, Icon, Divider, Search, Button, Dropdown, Table, Pagination } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 import './Styles.css';
 import windowSize from 'react-window-size';
+
+const index = 5;
 
 class History extends Component {
 
   constructor(props){
     super(props);
     this.state = {
+      activePage: 1,
+      start: 0,
+      end: index,
     }
+    this.handlePaginationChange = this.handlePaginationChange.bind(this);
   }
+
+  handlePaginationChange = (e, { activePage }) => 
+      {
+        this.setState({ 
+            activePage, 
+            start: (activePage - 1) * index,
+            end: activePage * index
+          })
+      }
 
   render(){
     return (
@@ -32,7 +47,7 @@ class History extends Component {
           </Table.Header>
           <Table.Body>
 
-              {this.props.history.slice(0, 25).map((l) => (
+              {this.props.history.slice(this.state.start, this.state.end).map((l) => (
                 <Table.Row>
                   <Table.Cell>{l.match}</Table.Cell>
                   <Table.Cell>{l.won} {" ( "}
@@ -48,6 +63,19 @@ class History extends Component {
 
           </Table.Body>
         </Table>
+
+        {this.props.history.length > index ?
+          <div style = {{textAlign: "left"}}>
+            <Pagination
+              boundaryRange={0}
+              ellipsisItem={null}
+              firstItem={null}
+              lastItem={null}
+              activePage={this.state.activePage}
+              onPageChange={this.handlePaginationChange}
+              totalPages={Math.ceil(this.props.history.length / index)}
+            />
+        </div> :null}
 
       </div>
 
